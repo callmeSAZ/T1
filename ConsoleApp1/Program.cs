71d59ea224fede32c8de43f0d71894b4cc4cd421
callmeSAZ <121518844+callmeSAZ@users.noreply.github.com>
2023-03-06 19:10:21 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y, z;
+            x = float.Parse(Console.ReadLine());
+            y = float.Parse(Console.ReadLine());
+            z = float.Parse(Console.ReadLine());
+
+            float sum = x + y + z;
+            float avg = sum / 3;
+
+            Console.WriteLine("Average={0}", avg);
+            Console.ReadKey();
+        }
+    }
+}
